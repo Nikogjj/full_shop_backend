f7aa974c99a7a67451578bd89a151927df13a05c
Niko <nikolaguemmoud@hotmail.fr>
2025-06-02 15:23:04 +0200

    addCategoryToBDD

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categories;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    //
+    /**
+     * Ajouter une nouvelle catégorie à la base de données.
+     * @param \Illuminate\Http\Request $request
+     * @return mixed|\Illuminate\Http\JsonResponse
+     */
+    public function addCategoryToBDD(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'parent_id' => 'nullable|exists:categories,id',
+            ]);
+    
+            // Je crée une nouvelle catégorie
+            $category = Categories::create([
+                "name" => $request->name,
+                "parent_id"=> $request->parent_id,
+            ]);
+            $category->save();
+            return response()->json(['message' => 'Category added successfully', 'category' => $category], 201);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Erreur durant la création de la catégorie :' . $th->getMessage(),
+            ], 500);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\ChampionnatsController;
 use App\Http\Controllers\ProductsController;
 use App\Models\User;
@@ -10,8 +11,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get("get_all_championnats", [ChampionnatsController::class,"getAllChampionnats"]);
 
 // A FAIRE ROUTE ADMIN
-Route::post("add_championnat",[ChampionnatsController::class,"addChampionnatToBDD"]);
-Route::post("add_team_to_championnat",[ChampionnatsController::class,"addTeamToChampionnat"]);
+Route::post("add_category",[CategoriesController::class,"addCategoryToBDD"]);
